03c7e7cca8b357a73530bee8302d1f72bf2f9e3f
anoint <dbwlsk870@hanmail.net>
2019-09-16 21:28:11 +0900

    Login, SignUp 화면만 작업

--- frontEnd/muscat/src/App.vue
+++ frontEnd/muscat/src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <Login /> -->
+    <SignUp />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    // Login
+    SignUp
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
